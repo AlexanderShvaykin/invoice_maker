0f99d54243f42b5cbc268bddeea0860c996303c6
Alexander Shvaykin <skiline.alex@gmail.com>
2020-07-13 00:02:03 +0300

    fix migration

--- db/migrate/20200711123305_add_not_nullto_order.rb
+++ db/migrate/20200711123305_add_not_nullto_order.rb
@@ -1,6 +1,6 @@
 class AddNotNulltoOrder < ActiveRecord::Migration[6.0]
   def change
-    change_column_null :invoices, :bank_id, false
+    change_column_null :orders, :bank_id, false
     remove_column :organizations, :bank_id
   end
 end
