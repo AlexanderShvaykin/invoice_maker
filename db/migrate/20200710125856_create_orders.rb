fcd8aee6b534850e197d2a2e49a9ad390cb34310
Alexander Shvaykin <skiline.alex@gmail.com>
2020-07-12 23:04:33 +0300

    fix migration

--- db/migrate/20200710125856_create_orders.rb
+++ db/migrate/20200710125856_create_orders.rb
@@ -1,6 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
-    create_table :invoices do |t|
+    create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.references :organization, null: false, foreign_key: true
       t.string :number
