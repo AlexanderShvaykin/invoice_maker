488b54651cef72fcd01ff8e07a5defccea884b6e
Alexander Shvaykin <skiline.alex@gmail.com>
2020-07-12 23:58:01 +0300

    fix migration

--- db/migrate/20200711122955_add_bank_to_order.rb
+++ db/migrate/20200711122955_add_bank_to_order.rb
@@ -1,5 +1,5 @@
 class AddBankToOrder < ActiveRecord::Migration[6.0]
   def change
-    add_reference :invoices, :bank, foreign_key: true
+    add_reference :orders, :bank, foreign_key: true
   end
 end
